8f41ae020311b73f1e0b0ab8760e15de3c2bd651
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2020-12-02 19:08:24 -0500

    Updated the program to measure execution time and offer the option to hold the execution when done using an std::cin

--- VS_CPP/AdventOfCode/Source/AdventOfCode.cpp
+++ VS_CPP/AdventOfCode/Source/AdventOfCode.cpp
@@ -1,22 +1,54 @@
-
 #include "Generated/GeneratedDefinitions.h"
+#include <chrono>
 
-// EDIT HERE! These lines will change what is executed
+#define TIME_NOW(varName) std::chrono::high_resolution_clock::time_point const varName = std::chrono::high_resolution_clock::now()
+#define TIME_DIFF_MICRO(beginVarName, endVarName) (std::chrono::duration_cast<std::chrono::microseconds>(endVarName - beginVarName).count() / 1000000.f)
+
+
+// EDIT HERE:  These lines will change what is executed
 #define CHALLENGE_TYPE CChallenge_N
 #define IS_FIRST_PART true
+
+// EDIT_HERE:  Set this to true if your console closes before you have time to see your results at the end of the program
+#define HOLD_EXECUTION true
 
 
 
 int main()
 {
     CHALLENGE_TYPE challenge;
-    EErrorCode errorCode = challenge.SetUp(IS_FIRST_PART);
 
+    // Set up challenge program
+    TIME_NOW(setUpBegin);
+    EErrorCode errorCode = challenge.SetUp(IS_FIRST_PART);
+    TIME_NOW(setUpEnd);
+
+    // Execute challenge program
+    TIME_NOW(executionBegin);
     if (errorCode == EErrorCode::Success)
     {
         errorCode = challenge.Run();
     }
+    TIME_NOW(executionEnd);
 
+    // Clean up challange program
+    TIME_NOW(cleanUpBegin);
     challenge.CleanUp();
+    TIME_NOW(cleanUpEnd);
+
+    // Print timings
+    std::cout << std::endl << "<========>  DONE!  <========>" << std::endl;
+    std::cout << "  Set Up    :  " << TIME_DIFF_MICRO(setUpBegin, setUpEnd) << " s" << std::endl;
+    std::cout << "  Execution :  " << TIME_DIFF_MICRO(executionBegin, executionEnd) << " s" << std::endl;
+    std::cout << "  Clean Up  :  " << TIME_DIFF_MICRO(cleanUpBegin, cleanUpEnd) << " s" << std::endl;
+    std::cout << "<===========================>" << std::endl;
+
+    // Hold
+    if (HOLD_EXECUTION)
+    {
+        int hold;
+        std::cin >> hold;
+    }
+
     return static_cast<int>(errorCode);
 }
